657a804c8911cd1b3eb957284bc699e9609bbf72
jordancj <jordan-cj998@hotmail.com>
2025-07-17 20:57:21 +1000

    updated redirect
    Signed-off-by: jordancj <jordan-cj998@hotmail.com>

---
+++ frontend/src/Utils/redirectOldUrl.js
@@ -0,0 +1,8 @@
+const current =
+window.location.hostname
+const expected = 'jrfbattendanceappfd-d7bmgfb8f6g6debw.z03.azurefd.net'
+if(
+    current.endsWith('.azurestaticapps.net') && current !== expected
+    ){
+    window.location.href = `https://${expected}`
+    }
