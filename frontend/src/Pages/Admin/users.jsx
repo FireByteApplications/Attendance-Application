739a19f2407026b7f882fa9f9a253527296c9acc
jordancj <jordan-cj998@hotmail.com>
2025-07-21 15:45:51 +1000

    added table header to users to align
    Signed-off-by: jordancj <jordan-cj998@hotmail.com>

--- frontend/src/Pages/Admin/users.jsx
+++ frontend/src/Pages/Admin/users.jsx
@@ -162,6 +162,7 @@ export default function Users() {
         <table className="table table-striped mt-4">
           <thead>
             <tr>
+              <th> </th>
               <th>Name</th>
               <th>Fire Zone Number</th>
               <th>Status</th>
