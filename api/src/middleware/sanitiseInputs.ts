b945e946cf3ab2b2624c2e1fb847203fb1c5fac2
jordancj <jordan-cj998@hotmail.com>
2026-07-06 13:13:09 +1000

    Fix backend sanitisation

--- api/src/middleware/sanitiseInputs.ts
+++ api/src/middleware/sanitiseInputs.ts
@@ -529,7 +529,7 @@ export function sanitizeEventNumberQuery(
   res: Response,
   next: NextFunction
 ) {
-  const eventNumber = validator.trim(String(req.body.eventNumber ?? ""));
+  const eventNumber = validator.trim(String(req.query.eventNumber ?? ""));
 
   if (!isValidEventNumber(eventNumber)) {
     res.status(400).json({
@@ -538,7 +538,26 @@ export function sanitizeEventNumberQuery(
     return;
   }
 
-  req.body.eventNumber = eventNumber;
+  req.query.eventNumber = eventNumber;
+
+  next();
+}
+
+export function sanitizeEventNumberDelete(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const eventNumber = validator.trim(String(req.params.eventNumber ?? ""));
+
+  if (!isValidEventNumber(eventNumber)) {
+    res.status(400).json({
+      message: "Invalid event number.",
+    });
+    return;
+  }
+
+  req.params.eventNumber = eventNumber;
 
   next();
 }
