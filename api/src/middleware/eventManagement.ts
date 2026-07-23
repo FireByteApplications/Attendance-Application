85d25b830e7590f7516e91188981cf8342797471
FireByte Applications <53253862+FireByteApplications@users.noreply.github.com>
2026-07-23 15:39:54 +1000

    Feature/roles (#40)
    * update cookies
    
    * Fix typo in incident sanitisation
    
    * Added support for community engagement and removed old role mechanics
    
    * update deps
    
    * Eslint major update
    
    * Update Vite React Tooling
    
    * node types and typescript major updates
    
    * remove unused packages
    
    * Change from exceljs to write-excel-file
    
    * Update Lockfile
    
    * Update package file
    
    * Regenerate API Lockfile
    
    * Update sanitization and formatting to be consistent
    
    * Tighten sanistisation
    
    * Add different coloured navbar in dev
    
    * Update sanitisation, implement features and bug fixes, change dev navbar design
    
    * add required to fields
    
    * Fix event deletion bug, clean deletion button
    
    * add deletion for non incident events, fix sanitization
    
    * Fix backend sanitisation
    
    * Change role reporting formatting
    
    fix conflicts
    
    * Remove dev logs
    
    * cleanup
    
    * fix incorrect query
    
    * Add better csrf token management
    
    * Retrospective attendance from role assignment
    
    * Align eventType
    
    * Align with change in eventType value for incident
    
    ---------
    
    Signed-off-by: FireByte Applications <53253862+FireByteApplications@users.noreply.github.com>

--- api/src/middleware/eventManagement.ts
+++ api/src/middleware/eventManagement.ts
@@ -74,7 +74,7 @@ export function createEventService({
       }
 
       const incidentEvent = await eventsCollection.findOne({
-        eventType: "incident",
+        eventType: "Incident-Call",
         eventNumber
       });
 
